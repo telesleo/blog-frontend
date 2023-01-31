e009ddd9a2ba8b8191e7eb64cb3d10f599cf736b
Leo <leonardoandrei41@gmail.com>
2023-01-31 16:57:02 -0300

    Create new page "Post" for displaying a post by its id

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Posts from './pages/Posts';
+import Post from './pages/Post';
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Posts />} />
+        <Route path="/post/:id" element={<Post />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Post/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import request from '../../utils/request';
+
+export default function Post() {
+  const { id } = useParams();
+
+  const [post, setPost] = useState();
+
+  const getPost = async () => {
+    const postData = await request(`/posts/${id}`, {
+      method: 'GET',
+    });
+    setPost(postData);
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  const getDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = {
+      year: 'numeric', month: 'long', day: 'numeric',
+    };
+    return date.toLocaleDateString('en-US', options);
+  };
+
+  return (
+    (post) && (
+    <div>
+      <h2>{post.title}</h2>
+      <h4>{post.author.name}</h4>
+      <p>{post.author.username}</p>
+      <p>{getDate(post.created_at)}</p>
+      <p>{post.content}</p>
+    </div>
+    )
+  );
+}
