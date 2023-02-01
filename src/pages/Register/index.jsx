8b802ec87c49590fa5fd9b6aba32bb17a24db851
Leo <leonardoandrei41@gmail.com>
2023-02-01 20:36:05 -0300

    New function for checking if all input fields of Register are valid

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -23,16 +23,98 @@ export default function Register() {
     setState[target.id](target.value);
   };
 
+  const isEmailValid = () => {
+    if (!email) {
+      setErrorMessage('"Email" field is empty.');
+      return false;
+    }
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+      setErrorMessage('Email is not valid.');
+      return false;
+    }
+    return true;
+  };
+
+  const isPasswordValid = () => {
+    if (!password) {
+      setErrorMessage('"Password" field is empty.');
+      return false;
+    }
+    if (password.length < 8) {
+      setErrorMessage('Password is too short.');
+      return false;
+    }
+    if (password.length > 64) {
+      setErrorMessage('Password is too long.');
+      return false;
+    }
+    if (!/^[a-zA-Z0-9#&*@]*$/.test(password)) {
+      setErrorMessage('Password is not valid.');
+      return false;
+    }
+    return true;
+  };
+
+  const isNameValid = () => {
+    if (!name) {
+      setErrorMessage('"Name" field is empty.');
+      return false;
+    }
+    if (name.length < 3) {
+      setErrorMessage('Name is too short.');
+      return false;
+    }
+    if (name.length > 64) {
+      setErrorMessage('Name is too long.');
+      return false;
+    }
+    if (!/^[a-zA-Z]+(?:[ ]{1}[a-zA-Z]+)*$/.test(name)) {
+      setErrorMessage('Name is not valid.');
+      return false;
+    }
+    return true;
+  };
+
+  const isUsernameValid = () => {
+    if (!username) {
+      setErrorMessage('"Username" field is empty.');
+      return false;
+    }
+    if (username.length < 3) {
+      setErrorMessage('Username is too short.');
+      return false;
+    }
+    if (username.length > 64) {
+      setErrorMessage('Username is too long.');
+      return false;
+    }
+    if (!/^[a-zA-Z0-9#&*@]*$/.test(username)) {
+      setErrorMessage('Username is not valid.');
+      return false;
+    }
+    return true;
+  };
+
+  const validateFields = () => {
+    if (isEmailValid() && isPasswordValid() && isNameValid() && isUsernameValid()) {
+      setErrorMessage('');
+      return true;
+    }
+    return false;
+  };
+
   const register = async () => {
-    const response = await request('/users/register', 'POST', {
-      email, password, name, username,
-    });
-    if (response.message === 'User created successfully.') {
-      if ((await login(email, password))) {
-        navigate('/');
+    if (validateFields()) {
+      const response = await request('/users/register', 'POST', {
+        email, password, name, username,
+      });
+      if (response.message === 'User created successfully.') {
+        if ((await login(email, password))) {
+          navigate('/');
+        }
+      } else {
+        setErrorMessage(response.message);
       }
-    } else {
-      setErrorMessage(response.message);
     }
   };
 
