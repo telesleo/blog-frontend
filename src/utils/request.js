05c11d83418f8f3b7f69833005ab2b166312b80b
Leo <leonardoandrei41@gmail.com>
2023-02-12 19:36:11 -0300

    Add authorization header with access token to API requests

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,13 @@
 export default async function request(endpoint, method, body) {
+  const token = localStorage.getItem('access_token');
+
   const url = process.env.REACT_APP_API_URL + endpoint;
   const response = await fetch(url, {
     method,
     body: JSON.stringify(body),
     headers: {
       'Content-Type': 'application/json',
+      Authorization: token,
     },
   });
   const data = await response.json();
