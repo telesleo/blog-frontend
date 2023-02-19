1e98765631c880f56f8b798fae687dc7e9890958
leo <leonardoandrei41@gmail.com>
2023-02-19 12:30:11 -0300

    Add style to component Description

--- src/components/Description/index.jsx
+++ src/components/Description/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './style.module.css';
 
 export default function Description({ description }) {
   return (
-    <p>{description}</p>
+    <p title={description} className={styles.description}>{description}</p>
   );
 }
 
