62edeb32c2fb6340f334f70f4b35379f86cdafca
leo <leonardoandrei41@gmail.com>
2023-02-19 12:28:48 -0300

    Add style to component PostCard

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -3,16 +3,19 @@ import PropTypes from 'prop-types';
 import Title from '../Title';
 import Author from '../Author';
 import Description from '../Description';
+import styles from './style.module.css';
 
 export default function PostCard({
   id, title, username, description,
 }) {
   return (
-    <a href={`/post/${id}`}>
-      <Title title={title} />
-      <Author username={username} />
-      <Description description={description} />
-    </a>
+    <div className={styles['post-card']}>
+      <a href={`/post/${id}`}>
+        <Title title={title} />
+        <Author username={username} />
+        <Description description={description} />
+      </a>
+    </div>
   );
 }
 
