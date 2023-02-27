38d74c6c9f5c944a3562ab326c83822ba3d6e234
leo <leonardoandrei41@gmail.com>
2023-02-27 15:12:12 -0300

    Modify component Button to have id

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './style.module.css';
 
-export default function Button({ children, onClick }) {
+export default function Button({ children, id, onClick }) {
   return (
-    <button className={styles.button} type="submit" onClick={onClick}>{children}</button>
+    <button id={id} className={styles.button} type="submit" onClick={onClick}>{children}</button>
   );
 }
 
@@ -12,5 +12,6 @@ Button.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
   ]).isRequired,
+  id: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
 };
