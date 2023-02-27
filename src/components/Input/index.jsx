2e8935486d3543676075ef6f4e52b1bd26a6dc76
leo <leonardoandrei41@gmail.com>
2023-02-27 14:45:27 -0300

    Add style to component Input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './style.module.css';
 
-export default function Input({ placeholder, value, onChange }) {
+export default function Input({
+  type, placeholder, value, onChange,
+}) {
   return (
-    <input placeholder={placeholder} value={value} onChange={onChange} />
+    <input
+      className={styles.input}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
   );
 }
 
 Input.propTypes = {
+  type: PropTypes.string,
   placeholder: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
+};
+
+Input.defaultProps = {
+  type: 'text',
 };
