e0b5f49aacb635bff6df808d12bd46db5f5b949a
leo <leonardoandrei41@gmail.com>
2023-03-03 19:24:39 -0300

    Add style to page User

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Author from '../../components/Author';
 import Description from '../../components/Description';
 import PostList from '../../components/PostList';
 import request from '../../utils/request';
+import styles from './style.module.css';
 
 export default function User() {
   const { username } = useParams();
@@ -18,12 +20,18 @@ export default function User() {
   }, []);
 
   return (
-    <div>
-      <img src="/images/profile_picture.png" alt="profile" />
-      <h2>{user.name}</h2>
-      <h2>{user.username}</h2>
-      {(user.description) && <Description description={user.about} />}
-      {(user) && <PostList posts={user.posts} />}
+    <div id={styles.user}>
+      <section id={styles['user-section']}>
+        <img id={styles['profile-picture']} src="/images/profile_picture.png" alt="profile" />
+        <h2 id={styles.name}>{user.name}</h2>
+        <Author username={user.username} />
+        <div id={styles.description}>
+          <Description description={user.about} />
+        </div>
+      </section>
+      <section id={styles['posts-section']}>
+        {(user) && <PostList posts={user.posts} />}
+      </section>
     </div>
   );
 }
