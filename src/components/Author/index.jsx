ba7536b2200a307dc561105000cdacba63983439
leo <leonardoandrei41@gmail.com>
2023-05-26 09:48:53 -0300

    Redirect to the author's page when the username is clicked

--- src/components/Author/index.jsx
+++ src/components/Author/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import styles from './style.module.css';
 
 export default function Author({ username }) {
   return (
-    <h4 className={styles.author}>{`@${username}`}</h4>
+    <Link to={`/${username}`}>
+      <h4 className={styles.author}>{`@${username}`}</h4>
+    </Link>
   );
 }
 
