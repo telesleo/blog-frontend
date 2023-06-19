0d2b7273e93624a6c39fa38d34b76f97e997b7e6
leo <leonardoandrei41@gmail.com>
2023-06-19 17:16:34 -0300

    Fix comment list spacing between comments

--- src/components/CommentList/index.jsx
+++ src/components/CommentList/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Comment from '../Comment';
+import styles from './style.module.css';
 
 export default function CommentList({ comments }) {
   return (
-    <div>
+    <div id={styles['comment-list']}>
       {
         (comments) && (
           comments.map((comment) => <Comment key={comment.id} comment={comment} />)
