fad18b433ea4ce339b17716010087af22b15eafb
leo <leonardoandrei41@gmail.com>
2023-06-29 16:38:13 -0300

    CSS - Modify User page to work on large screens

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -22,18 +22,26 @@ export default function User() {
   return (
     <div id={styles.user}>
       <section id={styles['user-section']}>
-        <img id={styles['profile-picture']} src="/images/profile_picture.png" alt="profile" />
-        <h2 id={styles.name}>{user.name}</h2>
-        <Author username={user.username} />
-        { (user?.about) && (
-          <div id={styles.description}>
-            <Description description={user.about} />
+        <div id={styles['user-section-container']}>
+          <div id={styles['name-and-username-container']}>
+            <img id={styles['profile-picture']} src="/images/profile_picture.png" alt="profile" />
+            <div>
+              <h2 id={styles.name}>{user.name}</h2>
+              <Author username={user.username} />
+            </div>
           </div>
-        ) }
+          { (user?.about) && (
+            <div id={styles.description}>
+              <Description description={user.about} />
+            </div>
+          ) }
+        </div>
       </section>
       { (user?.posts?.length > 0) && (
         <section id={styles['posts-section']}>
-          {(user) && <PostList posts={user.posts} />}
+          <div id={styles['posts-section-container']}>
+            {(user) && <PostList posts={user.posts} />}
+          </div>
         </section>
       ) }
     </div>
